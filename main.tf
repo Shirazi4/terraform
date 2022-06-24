7354ef057c10d746d3b4048b9eca96bf08b065ff
farwa <sfarwaabbas411@gmail.com>
2022-06-24 17:05:40 -0400

    b3333 branch new changes

--- main.tf
+++ main.tf
@@ -23,3 +23,29 @@ resource "aws_lb" "test" {
     prefix  = "test-lb"
     enabled = true
   }
+
+  resource "aws_lb" "test" {
+  name               = "test-lb-tf"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.lb_sg.id]
+  subnets            = [for subnet in aws_subnet.public : subnet.id]
+
+
+
+  resource "aws_lb" "test" {
+  name               = "test-lb-tf"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.lb_sg.id]
+  subnets            = [for subnet in aws_subnet.public : subnet.id]
+
+
+
+
+  resource "aws_lb" "test" {
+  name               = "test-lb-tf"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.lb_sg.id]
+  subnets            = [for subnet in aws_subnet.public : subnet.id]
